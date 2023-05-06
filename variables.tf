0d3fb451d23e3352753b4e327ef4a9b44c11ec41
Kevin Rocha Stettner <38251063+Dexstettner@users.noreply.github.com>
2023-05-06 13:04:09 -0300

    add zone variable

--- variables.tf
+++ variables.tf
@@ -6,6 +6,12 @@ variable "project_name" {
 variable "region" {
   description = "Google Cloud region"
   type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Google Cloud zone"
+  type        = string
   default     = "us-central1-a"
 }
 
